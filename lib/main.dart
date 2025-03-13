23419fbe9da0fa57b1569eb35e585d8c5aeebd9e
Chris Zaharia <czaharia@nyit.edu>
2025-03-13 12:20:04 -0400

    added expense model and expenses page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(),
+    const MaterialApp(
+      home: Expenses(),
+    ),
   );
 }

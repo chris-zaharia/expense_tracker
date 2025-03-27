9f6a85d88bc190e6160cae9bdf106f4054ac1a9b
Chris Zaharia <czaharia@nyit.edu>
2025-03-27 11:12:28 -0400

    expenses & expenses_list updates

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,22 +1,38 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
-class Expenses extends StatefulWidget{
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
-  
+
   @override
   State<Expenses> createState() {
     return _ExpensesState();
   }
 }
 
-class _ExpensesState extends State<Expenses>{
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Burger',
+        amount: 11.99,
+        date: DateTime.now(),
+        category: Category.food,
+        ),
+        Expense(
+        title: 'Laptop',
+        amount: 1099.99,
+        date: DateTime.now(),
+        category: Category.work,
+        ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
           Text("Chart"),
-          Text('Expenses List...'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses))
         ],
       ),
     );

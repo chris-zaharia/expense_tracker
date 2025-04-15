f87ade76d686373b52771886f4e155edb27e20cb
Chris Zaharia <czaharia@nyit.edu>
2025-04-15 12:13:39 -0400

    date picker, category picker

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
   @override
   State<NewExpense> createState() {
-    // TODO: implement createState
     return _NewExpenseState();
   }
 }
@@ -13,10 +16,28 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category? _selectedCategory = Category.leisure;
+  //Controller for amount
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
+    //Make sure to dispose of it
     super.dispose();
+  }
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
   }
 
   @override
@@ -32,39 +53,77 @@ class _NewExpenseState extends State<NewExpense> {
                   controller: _titleController,
                   maxLength: 50,
                   keyboardType: TextInputType.name,
-                  decoration: const InputDecoration(
+                  decoration: InputDecoration(
                     label: Text('Title'),
                   ),
                 ),
-              )
+              ),
             ],
           ),
           Row(
             children: [
-              SizedBox(
-                width: 120,
+              Expanded(
                 child: TextField(
                   controller: _amountController,
                   keyboardType: TextInputType.number,
                   decoration: const InputDecoration(
-                    prefixText: '\$ ',
-                    label: Text('Amount'),
+                    prefixText: '\$',
+                    label: Text("Amount"),
                   ),
                 ),
               ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(_selectedDate == null
+                        ? 'Selected Date'
+                        : formatter.format(_selectedDate!)),
+                    IconButton(
+                        onPressed: _presentDatePicker,
+                        icon: const Icon(Icons.calendar_month)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(
+                            category.name.toString().toUpperCase(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {}),
             ],
           ),
           Row(
             children: [
               ElevatedButton(
                 onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel"),
+              ),
+              ElevatedButton(
+                onPressed: () {
                   print(_titleController.text);
                   print(_amountController.text);
+                  //print the amount data
                 },
-                child: Text("Save Expense"),
+                child: Text('Save Expense'),
               ),
             ],
-          )
+          ),
         ],
       ),
     );
